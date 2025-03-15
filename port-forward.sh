d7687f6c9d049371058de62383da6449e1da398f
Talha Asghar <153183682+Talha-ji@users.noreply.github.com>
2025-03-15 12:40:47 +0500

    Create port-forward.sh

---
+++ port-forward.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+while true; do
+  # Get the name of the latest hello-world pod
+  POD_NAME=$(kubectl get pods -l app=hello-world -o jsonpath="{.items[0].metadata.name}")
+
+  # Start port-forwarding
+  echo "Starting port-forward for pod: $POD_NAME"
+  kubectl port-forward svc/hello-world 3000:5678 --address 0.0.0.0
+
+  # If port-forwarding fails, wait for a few seconds and retry
+  echo "Port-forwarding failed. Retrying in 5 seconds..."
+  sleep 5
+done
